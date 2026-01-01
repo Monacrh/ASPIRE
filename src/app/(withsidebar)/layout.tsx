08e79e9fdd997faba948c0a04512bc5e95cca4d1
Monarch <bryandhaniel123@gmail.com>
2026-01-01 10:25:50 +0700

    feat: remove dark mode to keep text black

--- src/app/(withsidebar)/layout.tsx
+++ src/app/(withsidebar)/layout.tsx
@@ -8,11 +8,8 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex min-h-screen bg-[#FFF8DC] overflow-x-hidden">
-      {/* Sidebar Component */}
       <RetroSidebar />
       
-      {/* Main Content Area */}
-      {/* pt-16 ditambahkan untuk memberi ruang pada Mobile Trigger Button yang fixed di kiri atas */}
       <main className="flex-1 p-4 md:p-8 overflow-y-auto h-screen w-full pt-20 md:pt-8">
         <div className="max-w-5xl mx-auto">
           {children}
